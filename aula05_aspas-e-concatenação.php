49d7eb67682ca6591bbd2f09293aa544122debdc
Felipe da Cruz <bsb.felipecorrea@gmail.com>
2021-04-11 20:08:51 -0300

    Update aula05_aspas-e-concatenação.php

--- aula05_aspas-e-concatenação.php
+++ aula05_aspas-e-concatenação.php
@@ -1,8 +1,7 @@
 <?php
 //Concatenação
 $nome = "Felipe";
-//use o "." para concatenar
+//use o "." para concatenar - aspas simples interpreta tudo como string.
 echo 'Meu nome é '.$nome.' e sou 10.<br>';
 //aspas duplas convertem automático
 echo "Meu nome é $nome.";
-?>
