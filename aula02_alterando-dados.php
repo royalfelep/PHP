4dbb366d12f83df0f42289ecbef3195aa4a5d076
Felipe da Cruz <bsb.felipecorrea@gmail.com>
2021-04-11 20:08:02 -0300

    Update aula02_alterando-dados.php

--- aula02_alterando-dados.php
+++ aula02_alterando-dados.php
@@ -3,9 +3,10 @@
 $nome = "Felipe";
 $profissao = "estudante";
 $sexo = "masculino";
+$idade = 21
 
 // Printando dados
-echo "Meu nome é $nome, sou $profissao e do sexo $sexo.<br>";
+echo "Meu nome é $nome, tenho $idade, sou $profissao e do sexo $sexo.<br>";
 // Alterando dados
 $nome = "Felipão";
 $profissao = "milionário";
