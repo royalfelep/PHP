fe6f5798e621cb04530b6d8f44ec3855472cbad4
Felipe da Cruz <bsb.felipecorrea@gmail.com>
2021-04-11 20:08:08 -0300

    Update aula04_tipos-de-dados.php

--- aula04_tipos-de-dados.php
+++ aula04_tipos-de-dados.php
@@ -1,20 +1,20 @@
 <?php
 //str
 $teste = "Teste um";
-var_dump ($teste);
+var_dump($teste);
 
-if(is_string($teste)):
+if (is_string($teste)) :
     echo "Positivo para string.";
-else:
+else :
     echo "Negativo para string.";
 endif;
 
 $teste = 123;
-var_dump ($teste);
+var_dump($teste);
 
-if(is_string($teste)):
+if (is_string($teste)) :
     echo "Positivo para string.";
-else:
+else :
     echo "Negativo para string.";
 endif;
 
@@ -24,9 +24,9 @@ echo "<hr>";
 $idade = 21;
 var_dump($idade);
 
-if(is_int($idade)):
-    print ("Positivo para inteiro");
-else:
+if (is_int($idade)) :
+    print("Positivo para inteiro");
+else :
     print("Negativo para inteiro");
 endif;
 
@@ -36,9 +36,9 @@ echo "<hr>";
 $altura = 8.9;
 var_dump($altura);
 
-if(is_float($altura)):
-    print ("Positivo para float");
-else:
+if (is_float($altura)) :
+    print("Positivo para float");
+else :
     print("Negativo para float");
 endif;
 
@@ -48,30 +48,32 @@ echo "<hr>";
 $eu = false;
 var_dump($eu);
 
-if(is_bool($eu)):
-    print ("Positivo para boolean");
-else:
-    print ("Negativo para boolean");
+if (is_bool($eu)) :
+    print("Positivo para boolean");
+else :
+    print("Negativo para boolean");
 endif;
 
 echo "<hr>";
 
 //array
-$etc = array("felipe",123,12.3,true);
+$etc = array("felipe", 123, 12.3, true);
 var_dump($etc);
 
-if(is_array($etc)):
-    print ("Positivo para array");
-else:
-    print ("Netagitvo para array");
+if (is_array($etc)) :
+    print("Positivo para array");
+else :
+    print("Netagitvo para array");
 endif;
 
 echo "<hr>";
 
 //object
-class pessoa {
+class pessoa
+{
     public $nome;
-    public function atribuirNome($nome) {
+    public function atribuirNome($nome)
+    {
         $this->$nome = $nome;
     }
 }
@@ -79,10 +81,10 @@ $pessoa = new pessoa();
 $pessoa->atribuirNome("Felipe");
 var_dump($pessoa);
 
-if(is_object($pessoa)):
-    print ("Positivo para object");
-else:
-    print ("Netagitvo para object");
+if (is_object($pessoa)) :
+    print("Positivo para object");
+else :
+    print("Netagitvo para object");
 endif;
 
 echo "<hr>";
@@ -90,4 +92,3 @@ echo "<hr>";
 //NULL
 $valor = NULL;
 var_dump($valor);
-?>
