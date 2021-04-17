3ae39a0383dea8a1c25eea353198d701f4d7f0c9
Felipe da Cruz <bsb.felipecorrea@gmail.com>
2021-04-16 22:16:13 -0300

    Update aula09_funcoes-de-arrays.php

--- aula09_funcoes-de-arrays.php
+++ aula09_funcoes-de-arrays.php
@@ -11,3 +11,29 @@ echo "<hr>";
 // array_values cria um novo array com novos indices e mantendo os valores
 $values = array_values($pessoas_de_lugares);
 print_r($values);
+echo "<hr>";
+
+// array_merge cria um novo array com os dois arrays passados
+$paises = array("brasil", "frança", "china");
+$comidas = array("churrasco", "pizza", "sushi");
+$mundo = array_merge($paises, $comidas);
+print_r($mundo);
+echo "<hr>";
+
+// array_pop exclui o elemento na ultima posição do array
+array_pop($mundo);
+print_r($mundo);
+echo "<br>";
+// array_pop atribui a variavel o valor do ultimo elemento do array
+$novomundo = array_pop($mundo);
+print_r($novomundo);
+echo "<hr>";
+
+// array_shift exclui o elemento na primeira posição do array
+array_shift($mundo);
+print_r($mundo);
+echo "<br>";
+// array_shift atribui a variavel o valor do primeiro elemento do array
+$novomundo = array_shift($mundo);
+print_r($novomundo);
+echo "<hr>";
